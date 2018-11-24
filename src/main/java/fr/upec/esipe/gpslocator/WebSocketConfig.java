0df921e7aa6cf31792d7f710b2d475be0593b1a4
esuau <evan.suau@etu.u-pec.fr>
2018-11-24 18:13:30 +0100

    fix(websocket): Enable cross origin websocket

--- src/main/java/fr/upec/esipe/gpslocator/WebSocketConfig.java
+++ src/main/java/fr/upec/esipe/gpslocator/WebSocketConfig.java
@@ -2,11 +2,13 @@ package fr.upec.esipe.gpslocator;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableWebSocket
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
@@ -18,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/locations");
+        registry.addEndpoint("/locations").setAllowedOrigins("*").withSockJS();
     }
 
 }
